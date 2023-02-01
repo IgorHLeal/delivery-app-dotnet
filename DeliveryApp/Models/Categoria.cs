8b4955348a452e97b18a4641b0d8a6b28940dbe8
Igor <igorhenriquelel3@gmail.com>
2023-02-01 17:04:37 -0300

    feat: Criação do Contexto e padronizção da convenção de utilização de namespaces

--- DeliveryApp/Models/Categoria.cs
+++ DeliveryApp/Models/Categoria.cs
@@ -1,11 +1,9 @@
-﻿namespace DeliveryApp.Models
+﻿namespace DeliveryApp.Models;
+public class Categoria
 {
-    public class Categoria
-    {
-        public int CategoriaId { get; set; }
-        public string CategoriaNome { get; set; }
-        public string Descricao { get; set; }
+    public int CategoriaId { get; set; }
+    public string CategoriaNome { get; set; }
+    public string Descricao { get; set; }
 
-        public List<Lanche> Lanches { get; set; }
-    }
+    public List<Lanche> Lanches { get; set; }
 }
