326a625462d18a07b16b4d5eb5ff81de40b1a319
Igor Leal <igorhenriqueleal3@gmail.com>
2023-02-02 16:46:11 -0300

    feat: Implementando o padrão Repository para Categoria

---
+++ DeliveryApp/Repositories/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using DeliveryApp.Models;
+
+namespace DeliveryApp.Repositories.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        IEnumerable<Categoria> Categorias { get; }
+    }
+}
