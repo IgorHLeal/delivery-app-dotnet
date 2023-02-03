099cab6faf732c7083448c5ec24bf0ef8984284e
Igor Leal <igorhenriqueleal3@gmail.com>
2023-02-03 14:26:28 -0300

    feat: Criado o Controller e view para Lanches

---
+++ DeliveryApp/Controllers/LancheController.cs
@@ -0,0 +1,21 @@
+﻿using DeliveryApp.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeliveryApp.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lacheRepository;
+
+        public LancheController(ILancheRepository lacheRepository)
+        {
+            _lacheRepository = lacheRepository;
+        }
+
+        public IActionResult List()
+        {
+            var lanches = _lacheRepository.Lanches;
+            return View(lanches);
+        }
+    }
+}
