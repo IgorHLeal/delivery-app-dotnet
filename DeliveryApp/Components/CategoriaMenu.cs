4ffe857d8dbc7e677ceed32ec1adedb7ee4bfb10
Igor Leal <igorhenriqueleal3@gmail.com>
2023-03-01 20:11:01 -0300

    feat: Criando uma viewComponent para exibir os lanches por categoria em um dropdown

---
+++ DeliveryApp/Components/CategoriaMenu.cs
@@ -0,0 +1,21 @@
+﻿using DeliveryApp.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeliveryApp.Components
+{
+    public class CategoriaMenu : ViewComponent
+    {
+        private readonly ICategoryRepository _categoriaRepository;
+
+        public CategoriaMenu(ICategoryRepository categoriaRepository)
+        {
+            _categoriaRepository = categoriaRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categorias = _categoriaRepository.Categorias.OrderBy(categoria => categoria.CategoriaNome);
+            return View(categorias);
+        }
+    }
+}
